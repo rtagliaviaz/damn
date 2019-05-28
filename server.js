9f6492606ea1114e33f57f4885796fe016e1e358
robert <robert5638@gmail.com>
2019-05-27 21:17:43 -0400

    first

--- server.js
+++ server.js
@@ -7,20 +7,14 @@ const app = express();
 
 app.use(fileUpload());
 
-// serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.static('client/build'));
-
-  app.get('/*', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-} else {
-  app.use(express.static(path.join(__dirname, '/client/public')));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
-  });
-}
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // Upload Endpoint
 app.post('/upload', (req, res) => {
