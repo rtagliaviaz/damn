897ae54d88e95d3a03c9186077535def0b8b684b
robert <robert5638@gmail.com>
2019-05-28 07:59:58 -0400

    second

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
   });
 } 
 
