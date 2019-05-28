00dff5f15ec705a621eece361891dc1edd9e54a3
robert <robert5638@gmail.com>
2019-05-28 08:59:27 -0400

    ahora

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+
+app.use(fileUpload());
+
+
+// Upload Endpoint
+app.post('/upload', (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });    
+  }
+  
+  const file = req.files.file;
+  const fileMimeType = req.files.file.mimetype; 
+ 
+  
+//comparing mime-type
+  if ( (fileMimeType !== 'image/jpeg') && (fileMimeType != 'image/png') )  {    
+    return res.status(400).json({ msg: 'The extension file must be ".jpg", ".png" or ".jpeg"' });
+  } else {  
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send(err);
+      }
+
+      res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+        
+    });
+  }
+});
+
+//listen server
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on Port ${port}`));
+
+
