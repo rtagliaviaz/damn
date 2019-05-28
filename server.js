13c580fac84aec6196ec0c4fe2dc73cc8c99262f
robert <robert5638@gmail.com>
2019-05-28 09:01:15 -0400

    ok

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const app = express();
 app.use(fileUpload());
 
 
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+} 
+
 // Upload Endpoint
 app.post('/upload', (req, res) => {
   if (req.files === null) {
